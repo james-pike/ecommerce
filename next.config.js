797b7c368e211dc1ebbbcd013ab127d36b8e522e
James Pike <jamesandrewpike@gmail.com>
2023-04-27 12:01:31 -0400

    Disable Linter

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  eslint: { ignoreDuringBuilds: true },
+  // your other settings here ...
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -186,7 +186,7 @@ const footerNavigation = {
   ],
 }
 
-function classNames(...classes) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
